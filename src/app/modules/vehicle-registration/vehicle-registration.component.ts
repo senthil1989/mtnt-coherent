f80a3b5ed8fd6e6447a9c0018ee5eeca86b46d61
raghul <samraghull@gmail.com>
2019-07-20 17:24:53 +0530

    some css changes done date picker implemented to satisfy all scenarios

--- src/app/modules/vehicle-registration/vehicle-registration.component.ts
+++ src/app/modules/vehicle-registration/vehicle-registration.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { ApiService } from '../../core/services/api.service';
+// import { IMyDate } from './vehicle-registration-interface/vehicle-registration.interface';
 
 @Component({
   selector: 'app-vehicle-registration',
@@ -10,53 +12,98 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class VehicleRegistrationComponent implements OnInit {
   public startDateModel: any;
   public endDateModel: any;
-  public myDatePickerOptions = {
+  public myDatePickerOptions1 = {
     dateRange: false,
-    dateFormat: 'dd-mm-yyyy'
+    dateFormat: 'dd-mm-yyyy',
+    stylesData: {
+            selector: 'dp1',
+            styles: `
+                .dp1 {
+                    position: absolute !important;
+                    top : 0px;
+                    left : -121px;
+                }`
+        }
+        };
+  public myDatePickerOptions2 = {
+    dateRange: false,
+    dateFormat: 'dd-mm-yyyy',
+    stylesData: {
+            selector: 'dp1',
+            styles: `
+                .dp1 {
+                    position: absolute !important;
+                    top : 0px;
+                    left : -120px;
+                }`
+        },
+        disableUntil : {}
   };
   public registerForm: FormGroup;
   public submitted = false;
-  constructor(private formBuilder: FormBuilder) { }
+  public vendorList: any;
+  public selectedEndDate: Date;
+  public selectedStartDate: Date;
+  constructor(private formBuilder: FormBuilder,
+              private apiService: ApiService) { }
 
   ngOnInit() {
-    console.log('inside VehicleRegistrationComponent');
     this.registerForm = this.formBuilder.group({
-      vendorContact: ['', Validators.required],
-      logisticsContact: ['', Validators.required],
-      vehicleNumber: ['', Validators.required],
-      chassisNumber: ['', Validators.required],
-      typeofVehicle: ['', Validators.required],
-      insuranceNumber: ['', Validators.required],
-      roadWorthyNumber: ['', Validators.required],
-      formCorFormA: ['', Validators.required],
+      vendorName: ['', Validators.required],
+      vendorContact: ['', [Validators.required, Validators.minLength(0)]],
+      vendorLogisticsNumber: ['', [Validators.required, Validators.minLength(0), Validators.maxLength(20)]],
+      vehicleNumber: ['', [Validators.required, Validators.minLength(0), Validators.maxLength(45)]],
+      chassisNumber: ['', [Validators.required, Validators.minLength(0), Validators.maxLength(45)]],
+      typeOfVehicle: ['', [Validators.required, Validators.minLength(0), Validators.maxLength(20)]],
+      insuranceNumber: ['', [Validators.required, Validators.minLength(0)]],
+      roadWorthyNumber: ['', [Validators.required, Validators.minLength(0)]],
+      formCorAnumber: ['', [Validators.required, Validators.minLength(0)]],
       driverName: [''],
       driverContact: [''],
       licenseNumber: [''],
 
   });
-    const d: Date = new Date();
-    d.setDate(d.getDate() + 2);
-    this.startDateModel = this.endDateModel = {isRange: false, singleDate: {jsDate: d}, dateRange: null};
+    this.initDateModesls(new Date());
+    this.apiService.callGetAPI('../../../assets/json/vendorList.json').subscribe(data => {
+      this.vendorList = data['vendorList'];
+    });
+
   }
 
  get f() { return this.registerForm.controls; }
 
   onSubmit() {
     this.submitted = true;
-
-    // stop here if form is invalid
     if (this.registerForm.invalid) {
         return;
     }
 
     console.log(this.registerForm.value);
 }
+
+  initDateModesls(date: Date) {
+    this.selectedStartDate = new Date();
+    this.startDateModel = {isRange: false, singleDate: {jsDate: this.selectedStartDate}, dateRange: null};
+    const d: Date = date;
+    d.setDate(d.getDate() + 2);
+    this.selectedEndDate = d;
+    this.endDateModel = {isRange: false, singleDate: {jsDate: this.selectedEndDate}, dateRange: null};
+  }
   startDateChanged(e) {
-    console.log(e);
+    if (e.singleDate.jsDate > this.selectedEndDate) {
+      this.initDateModesls(e.singleDate.jsDate);
+    }
+    this.endDateDisable(e.singleDate.date.year, e.singleDate.date.month, e.singleDate.date.day);
+    this.selectedStartDate = e.singleDate.jsDate;
   }
 
   endDateChanged(e) {
-    console.log(e);
+    this.selectedEndDate = e.singleDate.jsDate;
   }
 
+  endDateDisable(year: number, month: number, day: number) {
+    this.myDatePickerOptions2.disableUntil['year'] = year;
+    this.myDatePickerOptions2.disableUntil['month'] = month;
+    this.myDatePickerOptions2.disableUntil['day'] = day;
+  }
 }
