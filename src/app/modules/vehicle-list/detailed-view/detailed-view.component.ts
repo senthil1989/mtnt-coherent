477d403528f66c96c22cfa819f6550248ba898b3
raghul <samraghull@gmail.com>
2019-07-28 03:20:51 +0530

    build issues fixed

--- src/app/modules/vehicle-list/detailed-view/detailed-view.component.ts
+++ src/app/modules/vehicle-list/detailed-view/detailed-view.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
+interface VehicleInterface {
+  vendorname: string;
+  registeredDate: number;
+  vehicleNumber: number;
+  typeOfVehicle: string;
+  vendorContact: number;
+  vehicleStatus: string;
+  vendorStatus: string;
+}
+
 @Component({
   selector: 'app-detailed-view',
   templateUrl: './detailed-view.component.html',
@@ -10,7 +20,7 @@ export class DetailedViewComponent implements OnInit {
   @Input() vehicleList;
   @Input() vehicleDetailIndex;
   @Output() detailedViewCloseEvent = new EventEmitter();
-  public vehicle: {};
+  public vehicle: VehicleInterface;
   constructor() { }
 
   ngOnInit() {
