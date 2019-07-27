a7125bf7dda0e6ad59e386be763bfde09d975283
raghul <samraghull@gmail.com>
2019-07-27 13:33:28 +0530

    period duration module added

---
+++ src/app/modules/period-duration/period-duration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-period-duration',
+  templateUrl: './period-duration.component.html',
+  styleUrls: ['./period-duration.component.scss']
+})
+export class PeriodDurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
