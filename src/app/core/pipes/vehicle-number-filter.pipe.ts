1655af7bbc40fd97a732ea92b4cf43139ba3a09c
raghul <samraghull@gmail.com>
2019-07-28 02:55:21 +0530

    vehicle list table filter completed and some ui fixes added

--- src/app/core/pipes/vehicle-number-filter.pipe.ts
+++ src/app/core/pipes/vehicle-number-filter.pipe.ts
@@ -5,17 +5,62 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false
 })
 export class VehicleNumberFilterPipe implements PipeTransform {
+  public finalValue: object[];
+  transform(value: any, selectTypeOfVehicle: string[], selectedVendorNames: string[], vehicleStatusSelect: string, vendorStatusSelect: string, searchedVehicleNumber: number): object[] {
+    this.finalValue = searchedVehicleNumber ? value.filter(val => val.vehicleNumber.startsWith(searchedVehicleNumber)) : value;
+    return this.customizedReturn(this.finalValue, selectTypeOfVehicle, selectedVendorNames, vehicleStatusSelect, vendorStatusSelect);
+  }
 
-  transform(value: any, selectedVendorNames: string[], searchedVehicleNumber: number): [] {
-    if (searchedVehicleNumber) {
-      return value.filter(val => val.vehicleNumber.startsWith(searchedVehicleNumber));
+  customizedReturn(value: any, selectTypeOfVehicle: string[], selectedVendorNames: string[], vehicleStatusSelect: string, vendorStatusSelect: string) {
+    if (selectedVendorNames.length > 0 || selectTypeOfVehicle.length > 0 || vehicleStatusSelect || vendorStatusSelect) {
+      return this.getFilteredVendorStatus(
+        this.getFilteredVehicleStatus(
+          this.getUniqueTypeOfVehicleFromTwoArrays(
+            this.getUniqueVendorNameFromTwoArrays(value, selectedVendorNames),
+            selectTypeOfVehicle), vehicleStatusSelect),
+        vendorStatusSelect);
     } else {
-      if (selectedVendorNames.length > 0) {
-      return value.filter(val => selectedVendorNames.indexOf(val.vendorname) === -1 );
-      } else {
-        return value;
-      }
+      return value;
     }
   }
 
+  getFilteredVehicleStatus(value: object[], vehicleStatusSelect: string) {
+    if (vehicleStatusSelect) {
+      return value.filter(val => val['vehicleStatus'] === vehicleStatusSelect);
+    } else {
+      return value;
+    }
+  }
+
+  getFilteredVendorStatus(value: object[], vendorStatusSelect: string) {
+    if (vendorStatusSelect) {
+      return value.filter(val => val['vendorStatus'] === vendorStatusSelect);
+    } else {
+      return value;
+    }
+  }
+
+  getUniqueVendorNameFromTwoArrays(arr1, arr2) {
+    const finalArray = [];
+    arr1.forEach(element1 => {
+      arr2.forEach(element2 => {
+        if (element1.vendorname === element2) {
+          finalArray.push(element1);
+        }
+      });
+    });
+  return finalArray.length > 0 ? finalArray : arr1;
+  }
+
+  getUniqueTypeOfVehicleFromTwoArrays(arr1, arr2) {
+    const finalArray = [];
+    arr1.forEach(element1 => {
+      arr2.forEach(element2 => {
+        if (element1.typeOfVehicle === element2) {
+          finalArray.push(element1);
+        }
+      });
+    });
+  return finalArray.length > 0 ? finalArray : arr1;
+  }
 }
